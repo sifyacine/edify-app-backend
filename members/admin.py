63a613e9de3f9346dee233dfc291c88b743be65b
YCN <89133149+sifyacine@users.noreply.github.com>
2024-09-25 17:47:58 +0100

    changes

--- members/admin.py
+++ members/admin.py
@@ -1,12 +1,4 @@
 from django.contrib import admin
 from .models import Member
 
-class MemberAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'group', 'joined_at', 'approved')  # Adjust fields as necessary
-    search_fields = ('user__username',)  # Example of searching by username if you have a User model related to Member
-
-# Unregister the existing Member model registration if it exists
-admin.site.unregister(Member)
-
-# Register the Member model with the custom admin class
-admin.site.register(Member, MemberAdmin)
+admin.site.register(Member)
