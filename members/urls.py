309ae941ea0f334107b2381b3d977568e356cbc0
YCN <89133149+sifyacine@users.noreply.github.com>
2024-09-28 14:40:43 +0100

    changes

--- members/urls.py
+++ members/urls.py
@@ -3,6 +3,6 @@ from .views import MemberCreateView, MemberDetailView, MemberListView
 
 urlpatterns = [
     path('create/', MemberCreateView.as_view(), name='create-member'),
-    path('<str:user__username>/', MemberDetailView.as_view(), name='member-detail'),  # Get member by username
+    path('<str:user__id/', MemberDetailView.as_view(), name='member-detail'),  # Get member by username
     path('list/', MemberListView.as_view(), name='member-list'),
 ]
